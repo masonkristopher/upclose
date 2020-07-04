a2012a73c1bbaa04118b19fd09a0e7498bfcd5f6
Richard Rosier <jimmyroro58@gmail.com>
2020-07-04 15:49:19 -0500

    install axios and ts-node; forntend button to check proxy

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const [test, setTest] = useState('');
+
+  const sendRequest = () => {
+    axios.get('/api')
+      .then(res => {
+        setTest(res.data);
+      })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {test && (
+          <div>{test}</div>
+        )}
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <button onClick={sendRequest}>send request</button>
         <a
           className="App-link"
           href="https://reactjs.org"
