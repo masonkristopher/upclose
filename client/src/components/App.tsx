4cb450b17ed02530d679b6cba39205ee134496f8
Richard Rosier <jimmyroro58@gmail.com>
2020-07-06 17:45:06 -0500

    add login and logout button to client

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import { GoogleLogout, GoogleLogin } from 'react-google-login';
 import Navbar from './Navbar';
 
 const App = () => {
+
+  const responseGoogle = (response: any) => {
+    console.log(response);
+  };
+
+  const logout = () => {
+    console.log('logged out');
+  };
+
   return (
     <div className="text-red-700">
       I am the almighty App
       <Navbar />
+      <GoogleLogin
+        clientId="619935015421-c9vv4mlcuabiotbke4dpnc2ehp760l3a.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        // isSignedIn={true}
+      />
+
+      <GoogleLogout
+        clientId="619935015421-c9vv4mlcuabiotbke4dpnc2ehp760l3a.apps.googleusercontent.com"
+        buttonText="Logout"
+        onLogoutSuccess={logout}
+      />
+
     </div>
   );
 };
--- server/app.ts
+++ server/app.ts
@@ -1,12 +1,47 @@
 import { Express, Request, Response } from 'express';
-// import path from 'path';
-// import express from 'express';
+// import session from 'express-session';
+import dotenv from 'dotenv';
+// import passport from 'passport';
+// import Auth0Strategy from 'passport-auth0';
+
+dotenv.config();
+
+// const sess = {
+//   secret: `${process.env.SESSION_SECRET}`,
+//   cookie: {},
+//   resave: true,
+//   saveUninitialized: true,
+// };
+
+// const strategy = new Auth0Strategy(
+//   {
+//     clientID: process.env.GOOGLE_CLIENT_ID,
+//     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     callbackURL:
+//       process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/',
+//   },
+//   (accessToken, refreshToken, extraParams, profile, done) => {
+//     return done(null, profile);
+//   },
+// );
 
 class Server {
   private app: Express;
 
   constructor(app: Express) {
     this.app = app;
+    // this.app.use(session(sess));
+    // passport.use(strategy);
+    // this.app.use(passport.initialize());
+    // this.app.use(passport.session());
+    // passport.serializeUser((user, done) => {
+    //   done(null, user);
+    // });
+
+    // passport.deserializeUser((user, done) => {
+    //   done(null, user);
+    // });
+
     this.app.get('/api', (req: Request, res: Response): void => {
       console.log('hit me');
       res.send('You have reached the API!');
