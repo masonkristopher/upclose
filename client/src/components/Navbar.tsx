04708f1ecefd4ec0ce2c6fe04a74354fbfb35e53
masonkristopher <masonk000@gmail.com>
2020-07-06 17:13:43 -0500

    add scaffolding for navbar/react router

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,17 +1,37 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
 import UserProfile from './UserProfile';
+import Neighborhood from './Neighborhood';
+import Messages from './Messages';
 
 const Navbar = () => {
   return (
-    <div className="text-blue">
-      I am the beautiful Navbar
-      <ul>
-        {/* link to UserProfile */}
-        {/* link to Neighborhood */}
-        {/* link to Logout */}
-        {/* link to Messages */}
-      </ul>
-    </div>
+    <Router>
+      <div className="text-blue">
+          <ul>
+            {/* link to UserProfile */}
+            <li><Link to="/profile">Profile</Link></li>
+            {/* link to Neighborhood */}
+            <li><Link to="/neighborhood">Neighborhood</Link></li>
+            {/* link to Logout */}
+            {/* <li><Link to="/logout">Logout</Link></li> */}
+            {/* link to Messages */}
+            <li><Link to="/messages">Messages</Link></li>
+          </ul>
+      </div>
+
+      <Switch>
+        <Route path="/profile"><UserProfile /></Route>
+        <Route path="/neighborhood"><Neighborhood /></Route>
+        {/* <Route path="/logout"><Logout /></Route> */}
+        <Route path="/messages"><Messages /></Route>
+      </Switch>
+    </Router>
   );
 };
 
