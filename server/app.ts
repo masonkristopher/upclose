4ad038721bdb6144c0bb3440483143b815baffa3
masonkristopher <masonk000@gmail.com>
2020-07-06 17:13:26 -0500

    enable server to serve static build files

--- server/app.ts
+++ server/app.ts
@@ -1,15 +1,24 @@
 import { Express, Request, Response } from 'express';
-// import path from 'path';
-// import express from 'express';
+import * as path from 'path';
+import express from 'express';
 
 class Server {
   private app: Express;
 
   constructor(app: Express) {
     this.app = app;
+
+    // use the static files from the build
+    this.app.use(express.static(`${path.resolve('./')}/client/build`));
+
     this.app.get('/api', (req: Request, res: Response): void => {
       console.log('hit me');
       res.send('You have reached the API!');
+    });
+
+    // for any requests to wildcard endpoints (from react router), server the static build files
+    this.app.get('*', (req: Request, res: Response): void => {
+      res.sendFile(`${path.resolve('./')}/client/build/index.html`);
     });
   }
 
