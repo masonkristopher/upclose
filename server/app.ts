4a2df1c24938a7035474fa5127f9c65e84c84e89
Richard Rosier <jimmyroro58@gmail.com>
2020-07-07 14:28:56 -0500

    add cookie parser middleware

--- server/app.ts
+++ server/app.ts
@@ -1,18 +1,19 @@
 import express, { Express, Request, Response } from 'express';
 import * as path from 'path';
-// import session from 'express-session';
+import cookieparser from 'cookie-parser';
+import session from 'express-session';
 import dotenv from 'dotenv';
-// import passport from 'passport';
-// import Auth0Strategy from 'passport-auth0';
+import passport from 'passport';
+import Auth0Strategy from 'passport-auth0';
 
 dotenv.config();
 
-// const sess = {
-//   secret: `${process.env.SESSION_SECRET}`,
-//   cookie: {},
-//   resave: true,
-//   saveUninitialized: true,
-// };
+const sess = {
+  secret: `${process.env.SESSION_SECRET}`,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+};
 
 // const strategy = new Auth0Strategy(
 //   {
@@ -31,28 +32,30 @@ class Server {
 
   constructor(app: Express) {
     this.app = app;
-    // this.app.use(session(sess));
+    this.app.use(cookieparser());
+    this.app.use(session(sess));
     // passport.use(strategy);
-    // this.app.use(passport.initialize());
-    // this.app.use(passport.session());
-    // passport.serializeUser((user, done) => {
-    //   done(null, user);
-    // });
+    this.app.use(passport.initialize());
+    this.app.use(passport.session());
+    passport.serializeUser((user, done) => {
+      done(null, user);
+    });
 
-    // passport.deserializeUser((user, done) => {
-    //   done(null, user);
-    // });
+    passport.deserializeUser((user, done) => {
+      done(null, user);
+    });
 
     // use the static files from the build
     this.app.use(express.static(`${path.resolve('./')}/client/build`));
-
-    this.app.get('/api', (req: Request, res: Response): void => {
-      console.log('hit me');
+    
+    this.app.get('/api', (req: any, res: Response): void => {
+      console.log(req);
       res.send('You have reached the API!');
     });
 
     // for any requests to wildcard endpoints (from react router), server the static build files
-    this.app.get('*', (req: Request, res: Response): void => {
+    this.app.get('*', (req: any, res: Response): void => {
+      console.log(req.session);
       res.sendFile(`${path.resolve('./')}/client/build/index.html`);
     });
   }
@@ -63,5 +66,9 @@ class Server {
     });
   }
 }
+
+
+ 
+
 
 export default Server;
