2266ba026d1104c58459c3487d91a943298097ad
Richard Rosier <jimmyroro58@gmail.com>
2020-07-07 14:30:10 -0500

    login and out with google through navbar; add debug launch.json

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,34 +1,24 @@
-import React from 'react';
-import { GoogleLogout, GoogleLogin } from 'react-google-login';
+import React, { useState } from 'react';
+import axios from 'axios';
 import Navbar from './Navbar';
 
 const App = () => {
+  const [user, setUser] = useState({});
 
-  const responseGoogle = (response: any) => {
-    console.log(response);
-  };
-
-  const logout = () => {
-    console.log('logged out');
-  };
+  const sendRequest = () => {
+    axios.get('/api')
+      .then((response) => { console.log(response); });
+  }
 
   return (
-    <div className="text-red-700">
-      I am the almighty App
-      <Navbar />
-      <GoogleLogin
-        clientId="619935015421-c9vv4mlcuabiotbke4dpnc2ehp760l3a.apps.googleusercontent.com"
-        buttonText="Login"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-        // isSignedIn={true}
-      />
-
-      <GoogleLogout
-        clientId="619935015421-c9vv4mlcuabiotbke4dpnc2ehp760l3a.apps.googleusercontent.com"
-        buttonText="Logout"
-        onLogoutSuccess={logout}
-      />
+    <div>
+      <button type="button" onClick={sendRequest}>send request</button>
+      <div className="text-red-700">
+        I am the almighty App
+        <Navbar
+          setAppsUser={setUser}
+        />
+      </div>
 
     </div>
   );
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -5,24 +5,64 @@ import {
   Route,
   Link,
 } from 'react-router-dom';
+import { GoogleLogout, GoogleLogin } from 'react-google-login';
 import UserProfile from './UserProfile';
 import Neighborhood from './Neighborhood';
 import Messages from './Messages';
 
-const Navbar = () => {
+const Navbar = ( {setAppsUser}:any ) => {
+  // triggers when a user successfully logs out; should alert the user and hit an endpoint in our server?
+  const logout = () => {
+    console.log('logged out');
+  };
+
+  // the response from google after the login
+  const responseGoogle = (response: any) => {
+    /* after a user successfully signs in,
+    send the user's ID token to your server using HTTPS.
+    axios.post('/verify', {
+      id_token: response.tokenId
+    })
+    Then, on the server, verify the integrity of the ID token and
+    axios.get('https://oauth2.googleapis.com/tokeninfo', {
+      id_token,
+    })
+    use the user information contained in the token to establish a session or create a new account.
+    alert the user that they have signed in, probs by changing state using setAppsUser and also setUser in Navbar?
+    */
+
+    console.log(response);
+  };
+
   return (
     <Router>
       <div className="text-blue">
-          <ul>
-            {/* link to UserProfile */}
-            <li><Link to="/profile">Profile</Link></li>
-            {/* link to Neighborhood */}
-            <li><Link to="/neighborhood">Neighborhood</Link></li>
-            {/* link to Logout */}
-            {/* <li><Link to="/logout">Logout</Link></li> */}
-            {/* link to Messages */}
-            <li><Link to="/messages">Messages</Link></li>
-          </ul>
+        <ul>
+          {/* link to UserProfile */}
+          <li><Link to="/profile">Profile</Link></li>
+          {/* link to Neighborhood */}
+          <li><Link to="/neighborhood">Neighborhood</Link></li>
+          {/* link to Logout */}
+          <li>
+            <GoogleLogout
+              clientId="619935015421-c9vv4mlcuabiotbke4dpnc2ehp760l3a.apps.googleusercontent.com"
+              buttonText="Logout"
+              onLogoutSuccess={logout}
+            />
+          </li>
+          <li>
+            <GoogleLogin
+              clientId="619935015421-c9vv4mlcuabiotbke4dpnc2ehp760l3a.apps.googleusercontent.com"
+              buttonText="Login"
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+            // isSignedIn={true}
+            />
+          </li>
+          {/* <li><Link to="/logout">Logout</Link></li> */}
+          {/* link to Messages */}
+          <li><Link to="/messages">Messages</Link></li>
+        </ul>
       </div>
 
       <Switch>
