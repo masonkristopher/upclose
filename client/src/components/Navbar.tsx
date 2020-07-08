501dc9b82f14057fbfb11e57b8f6607726bae93e
Richard Rosier <jimmyroro58@gmail.com>
2020-07-08 15:57:31 -0500

    make navbar pretty

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -10,13 +10,15 @@ import { GoogleLogout, GoogleLogin } from 'react-google-login';
 import UserProfile from './UserProfile';
 import Neighborhood from './Neighborhood';
 import Messages from './Messages';
+// import { UserContextConsumer } from './contexts/UserContext';
 
 const Navbar = ({
   // these come from App, and will alter App's user
   setUser,
   user,
-}:any) => {
-// triggers when a user successfully logs out; should alert the user and hit our server?
+}: any) => {
+
+  // triggers when a user successfully logs out; should alert the user and hit our server?
   const logout = () => {
     setUser(null);
     console.log('logged out');
@@ -36,50 +38,79 @@ const Navbar = ({
     // Then, on the server, verify the integrity of the ID token and
     // use the user information contained in the token to establish a session or create a new account.
     // alert the user that they have signed in, probs by changing state using setAppsUser and also setUser in Navbar?
-    
+
 
     console.log(response);
   };
 
   return (
     <Router>
-      <div className="text-blue">
-        {user && (
-          <div>Logged in as {user.username}</div>
-        )}
-        <ul>
-          {/* link to UserProfile */}
-          <li><Link to="/profile">Profile</Link></li>
-          {/* link to Neighborhood */}
-          <li><Link to="/neighborhood">Neighborhood</Link></li>
-          {/* link to Logout */}
-          <li>
-            <GoogleLogout
-              clientId="619935015421-c9vv4mlcuabiotbke4dpnc2ehp760l3a.apps.googleusercontent.com"
-              buttonText="Logout"
-              onLogoutSuccess={logout}
-            />
-          </li>
-          <li>
-            <GoogleLogin
-              clientId="619935015421-c9vv4mlcuabiotbke4dpnc2ehp760l3a.apps.googleusercontent.com"
-              buttonText="Login"
-              onSuccess={responseGoogle}
-              onFailure={responseGoogle}
-            // isSignedIn={true}
-            />
-          </li>
-          {/* <li><Link to="/logout">Logout</Link></li> */}
-          {/* link to Messages */}
-          <li><Link to="/messages">Messages</Link></li>
-        </ul>
+
+      <div className="flex-1 flex flex-col">
+        <nav className="px-4 flex justify-between bg-white h-16 border-b-2">
+          {/* <!-- top bar left --> */}
+          <ul className="flex items-center">
+            <li className="p-2"><Link to="/profile">Profile</Link></li>
+            <li className="p-2"><Link to="/neighborhood">Neighborhood</Link></li>
+            <li className="p-2"><Link to="/messages">Messages</Link></li>
+          </ul>
+
+          <ul className="flex items-center">
+            {/* in the middle */}
+            <li>
+              <h1 className="pl-5 lg:pl-0 text-gray-700">Im in the middle</h1>
+            </li>
+          </ul>
+          {/* <!-- to the right  --> */}
+          <ul className="flex items-center">
+            <li className="pr-2">
+              <GoogleLogout
+                clientId="619935015421-c9vv4mlcuabiotbke4dpnc2ehp760l3a.apps.googleusercontent.com"
+                buttonText="Logout"
+                onLogoutSuccess={logout}
+              />
+            </li>
+            <li className="pl-2">
+              <GoogleLogin
+                clientId="619935015421-c9vv4mlcuabiotbke4dpnc2ehp760l3a.apps.googleusercontent.com"
+                buttonText="Login"
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+              />
+            </li>
+            <li className="p-2">
+              {user && (
+                <div>
+                  <div>Logged in as {user.username}</div>
+                  <img
+                    className="rounded-full mx-auto h-6 w-6"
+                    src={user.avatar}
+                    alt="profile"
+                  />
+                </div>
+              )}
+            </li>
+          </ul>
+        </nav>
       </div>
 
       <Switch>
-        <Route path="/profile"><UserProfile /></Route>
-        <Route path="/neighborhood"><Neighborhood /></Route>
+        <Route path="/profile">
+          <UserProfile
+            user={user}
+          />
+        </Route>
+        <Route path="/neighborhood">
+          <Neighborhood 
+            user={user}
+          />
+        </Route>
         {/* <Route path="/logout"><Logout /></Route> */}
-        <Route path="/messages"><Messages /></Route>
+        <Route path="/messages">
+          <Messages 
+            user={user}
+          />
+          </Route>
       </Switch>
     </Router>
   );
