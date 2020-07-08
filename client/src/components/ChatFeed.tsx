72147a13df46d75bb308f3c228feda2f2c2586c0
Will Preble <wpreble1@gmail.com>
2020-07-08 17:31:40 -0500

    Scaffold party chat components

---
+++ client/src/components/ChatFeed.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// I am a child of HouseParty
+const ChatFeed = () => {
+  return (
+    <div className="text-blue">
+      I am the beautiful ChatFeed
+    </div>
+  );
+};
+
+export default ChatFeed;
