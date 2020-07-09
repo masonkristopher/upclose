031bc8eeae15020c9c222b4c8fb327c2b6e83fd3
Richard Rosier <jimmyroro58@gmail.com>
2020-07-09 17:45:18 -0500

    scaffold getAllParties method

--- server/db/methods.ts
+++ server/db/methods.ts
@@ -24,7 +24,13 @@ const getAllParties = async (id) => {
   try {
     // ****************************** to do: **************************************
     // we need to query our user/party join table and return all parties that match the user's id
-    // const parties =
+    // TABLENAME.findAll({where: {USER_ID: id}})
+      // .then((partyIds) => {
+      //   PARTIES.findall({where: {id}});
+      // })
+      // .then((parties) => {
+      //   return parties;
+      // })
   } catch (err) {
     console.error(err);
   }
