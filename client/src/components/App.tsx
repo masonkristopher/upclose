89c7d4f2a65fc7da840da865fd5bac26abcc49bb
NaseerHines <Hinesnaseer@gmail.com>
2020-07-09 16:53:36 -0500

    Fixed request on line 28 to return and send userObj

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import Navbar from "./Navbar";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Navbar from './Navbar';
 // import UserContext from './contexts/UserContext';
 // import SampleVidChat from './SampleVidChat';
 
@@ -10,12 +10,12 @@ const App = () => {
   useEffect(() => {
     // loads the gapi. we use (window as any) b/c gapi does not exist on window until our script
     // in public/index.html creates it
-    (window as any).gapi.load("auth2", () => {
+    (window as any).gapi.load('auth2', () => {
       // initializes the GoogleAuth object, which has all the fun methods we need
       (window as any).gapi.auth2
         .init({
           client_id:
-            "619935015421-c9vv4mlcuabiotbke4dpnc2ehp760l3a.apps.googleusercontent.com",
+            '619935015421-c9vv4mlcuabiotbke4dpnc2ehp760l3a.apps.googleusercontent.com',
         })
         .then(() => {
           // accesses GoogleAuth object and checks if someone is signed in. returns boolean
@@ -25,7 +25,7 @@ const App = () => {
               .getAuthInstance()
               .currentUser.get();
             // send the token to our server, which will verify it and give us the user from database
-            return axios.post("/user/verify", {
+            return axios.post('/user/verify', {
               userObj,
               id_token: userObj.wc.id_token,
             });
