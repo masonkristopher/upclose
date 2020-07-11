77f8253c57cc3aac402c5b41062c3badf889e86a
Richard Rosier <jimmyroro58@gmail.com>
2020-07-11 10:32:08 -0500

    show users associated with a party when PartyProfile.tsx loads

--- client/src/components/PartyProfile.tsx
+++ client/src/components/PartyProfile.tsx
@@ -19,6 +19,7 @@ interface PartyProfileProps {
 // clicking a house in the neighborhood will take you here, with the partyId
 const PartyProfile: FC<PartyProfileProps> = ({ user }) => {
   const [party, setParty]:any = useState({});
+  const [users, setUsers]:any = useState([]);
   // access the partyId from the route using useParams.
   const { partyId } = useParams();
 
@@ -28,6 +29,11 @@ const PartyProfile: FC<PartyProfileProps> = ({ user }) => {
       .then((response) => {
       // then use setParty to put the party's info into state
         setParty(response.data);
+        // should get all users that have joined this party
+        return axios.get(`/party/getUsers/${partyId}`);
+      })
+      .then((response) => {
+        setUsers(response.data);
       })
       .catch((err) => console.error(err));
   }, []);
@@ -36,11 +42,24 @@ const PartyProfile: FC<PartyProfileProps> = ({ user }) => {
     <div>
       <div className="text-blue">
         {party && (
-          <h4> 
+          <h4>
             party name is: {party.name}
           </h4>
         )}
+        {users && (
+          <h4>
+            Users involved in this party:
+            <ul>
+              {users.map((userInParty:any) => {
+                return (<li key={userInParty.username}>{userInParty.username}</li>);
+              })}
+            </ul>
+          </h4>
+        )}
       </div>
+      {/* {users.includes(user) === false && (
+        <button type="button"> Would you like to join this party?</button>
+      )} */}
       <button type="button">Change House layout</button>
       <button type="button">Change Party Settings</button>
 
