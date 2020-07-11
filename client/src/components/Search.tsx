36d02d11723301de949521caacdbd76f0069b841
NaseerHines <Hinesnaseer@gmail.com>
2020-07-11 14:01:52 -0500

    Search almost done grabsAllUsers and maps them as list

--- client/src/components/Search.tsx
+++ client/src/components/Search.tsx
@@ -1,20 +1,79 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const Search = () => {
+const Search = ({ partyId, setPartyUpdate, update }: any) => {
   const [input, setInput] = useState('');
   const [users, setUsers] = useState([]);
-  // const [state, setstate] = useState(initialState);
+  const [matches, setMatches]: any = useState([]);
 
   const getAllUsers = () => {
-    axios
+    return axios
       .get('/user/all')
-      .then((res) => console.log(res.data))
+      .then((res) => setUsers(res.data))
       .catch((err) => console.error(err));
   };
-  getAllUsers();
 
-  return <div className="text-blue">I am the Epic Search</div>;
+  useEffect(() => {
+    getAllUsers();
+  }, []);
+
+  const handleMatches = () => {
+  };
+
+  const inviteUser = (user: any) => {
+    axios
+      .post(`/user/${user.id}/joins/${partyId}`)
+      .then(() => {
+        // show user that they have joined in some way
+        setPartyUpdate(!update);
+      })
+      .catch((error) => console.error(error));
+  };
+
+  return (
+    <div className="text-blue">
+      I am the Epic Search
+      <div className="flex flex-wrap -mx-3 mb-6">
+        <div className="w-full px-3">
+          <input
+            onChange={(e) => setInput(e.target.value)}
+            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            id="grid-password"
+            type="text"
+            value={input}
+          />
+          <button
+            onClick={() => handleMatches()}
+            className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-blue-400 rounded shadow m-4"
+            type="button"
+          >
+            Search
+          </button>
+        </div>
+      </div>
+      <div>
+        {matches.length >= 1
+          ? matches.map((match: any) => (
+            <div>
+              <img
+                className="rounded-full mx-auto h-6 w-6"
+                src={match.item.avatar}
+                alt="avatar"
+              />
+              <li>{match.item.username}</li>
+              <button
+                onClick={() => inviteUser(match.item)}
+                className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-blue-400 rounded shadow m-4"
+                type="button"
+              >
+                Add
+              </button>
+            </div>
+          ))
+          : null}
+      </div>
+    </div>
+  );
 };
 
 export default Search;
