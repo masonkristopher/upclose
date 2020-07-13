262e35b918eaa840f7218a9ec3a2f588a33a8f13
NaseerHines <Hinesnaseer@gmail.com>
2020-07-13 17:57:11 -0500

    passes clickedUser and main user to view for p2p

--- client/src/components/Messages.tsx
+++ client/src/components/Messages.tsx
@@ -1,4 +1,6 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import MessagesView from './MessagesView';
+
 // maybe I am a popup, maybe a dropdown menu thing, maybe a separate page
 interface IProps {
   user:{
@@ -12,10 +14,22 @@ interface IProps {
   }
 }
 
-const Messages: FC<IProps> = () => {
+const Messages: FC<IProps> = ({ user }) => {
+  const [clickedUser, setClickedUser]: any = useState({});
+  const [messageView, setMessageView] = useState(false);
+  // const [] = useState();
+
   return (
-    <div className="text-blue">
-      I am the beautiful Messages
+    <div>
+      <div>
+        {messageView
+          ? <MessagesView clickedUser={clickedUser} user={user} />
+          : (
+            <div className="text-blue">
+              I am the beautiful Messages
+            </div>
+          )}
+      </div>
     </div>
   );
 };
