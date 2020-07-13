4a0a334be49627493b09793d123c2f90875b66dc
NaseerHines <Hinesnaseer@gmail.com>
2020-07-13 18:16:55 -0500

    trying to test hardcoded messageView on other computer

--- client/src/components/Messages.tsx
+++ client/src/components/Messages.tsx
@@ -15,13 +15,22 @@ interface IProps {
 }
 
 const Messages: FC<IProps> = ({ user }) => {
-  const [clickedUser, setClickedUser]: any = useState({});
+  // get rid of hard coded clickedUser
+  const [clickedUser, setClickedUser]: any = useState({
+    id: 2,
+    nameFirst: 'Travis',
+    nameLast: 'Scott',
+    username: 'AstroJack',
+    email: 'JackBoys@gmail.com',
+    avatar: 'https://i1.sndcdn.com/avatars-000701366305-hu9f0i-t500x500.jpg',
+    googleId: 'google ID here',
+  });
   const [messageView, setMessageView] = useState(false);
   // const [] = useState();
 
   return (
     <div>
-      <div>
+      {/* <div>
         {messageView
           ? <MessagesView clickedUser={clickedUser} user={user} />
           : (
@@ -29,7 +38,8 @@ const Messages: FC<IProps> = ({ user }) => {
               I am the beautiful Messages
             </div>
           )}
-      </div>
+      </div> */}
+      <MessagesView clickedUser={clickedUser} user={user} />
     </div>
   );
 };
