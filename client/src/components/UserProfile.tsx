4ce656fcf72ebeaade79071a45da25b48db8e6ed
Richard Rosier <jimmyroro58@gmail.com>
2020-07-13 10:42:20 -0500

    show parties a user is involved in in UserProfile.tsx

--- client/src/components/UserProfile.tsx
+++ client/src/components/UserProfile.tsx
@@ -1,8 +1,10 @@
-import React, { useState, FC } from 'react';
+import React, { useState, FC, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 import EditUserDetails from './EditUserDetails';
 
 interface UserProfileProps {
-  user:{
+  user: {
     id: number,
     nameFirst: string,
     nameLast: string,
@@ -16,41 +18,72 @@ interface UserProfileProps {
 
 const UserProfile: FC<UserProfileProps> = ({ user, setUser }) => {
   const [showEditForm, setShowEditForm]: any = useState(false);
+  const [parties, setParties]: any = useState([]);
+
+  const history = useHistory();
+
+  useEffect(() => {
+    axios.get(`/party/all/${user.id}`)
+      .then((response) => {
+        setParties(response.data);
+      })
+      .catch(err => console.error(err));
+  });
+
+  const toParty = (partyId: number) => {
+    history.replace(`/partyProfile/${partyId}`);
+  };
 
   return (
-    <div className="p-8 content-center">
-      {showEditForm ? (
-        <EditUserDetails setShowEditForm={setShowEditForm} user={user} setUser={setUser} />
-      ) : (
-        <div className="">
-          <img
-            className="flex-col object-cover rounded-full p-4"
-            src={user.avatar}
-            alt={user.username}
-          />
-          <h3>Username</h3>
-          <p className="text-gray-900 font-bold text-xl mb-2">
-            {user.username}
-          </p>
-          <h3>First Name</h3>
-          <p className="text-gray-900 font-bold text-xl mb-2">
-            {user.nameFirst}
-          </p>
-          <h3>Last Name</h3>
-          <p className="text-gray-900 font-bold text-xl mb-2">
-            {user.nameLast}
-          </p>
-          <h3>Email</h3>
-          <p className="text-gray-900 font-bold text-xl mb-2">{user.email}</p>
-          <button
-            onClick={() => setShowEditForm(!showEditForm)}
-            className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-blue-400 rounded shadow m-4"
-            type="button"
-          >
-            Edit User Details
-          </button>
-        </div>
-      )}
+    <div className="grid grid-cols-2">
+      <div className="p-8 content-center flex">
+        {showEditForm ? (
+          <EditUserDetails setShowEditForm={setShowEditForm} user={user} setUser={setUser} />
+        ) : (
+            <div>
+              <img
+                className="flex-col object-cover rounded-full p-4 max-w-sm max-h-sm"
+                src={user.avatar}
+                alt={user.username}
+              />
+              <h3>Username</h3>
+              <p className="text-gray-900 font-bold text-xl mb-2">
+                {user.username}
+              </p>
+              <h3>First Name</h3>
+              <p className="text-gray-900 font-bold text-xl mb-2">
+                {user.nameFirst}
+              </p>
+              <h3>Last Name</h3>
+              <p className="text-gray-900 font-bold text-xl mb-2">
+                {user.nameLast}
+              </p>
+              <h3>Email</h3>
+              <p className="text-gray-900 font-bold text-xl mb-2">{user.email}</p>
+              <button
+                onClick={() => setShowEditForm(!showEditForm)}
+                className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-blue-400 rounded shadow m-4"
+                type="button"
+              >
+                Edit User Details
+              </button>
+            </div>
+          )}
+      </div>
+      <div>
+        {parties && (
+          <h1>
+            Your parties:
+            <ul>
+              {parties.map((party: any) => (
+                <li>
+                  <button type="button" onClick={() => { toParty(party.id); }}>{party.name}</button>
+                </li>
+              ))}
+            </ul>
+          </h1>
+        )}
+      </div>
     </div>
   );
 };
