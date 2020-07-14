77cf289da6cf4c370d3d7c536091b6689e53e1b6
Clayton Christenson <cchristenson.opspark@gmail.com>
2020-07-14 14:42:12 -0500

    create inboxList component

---
+++ client/src/components/InboxList.tsx
@@ -0,0 +1,47 @@
+import React, { FC, useState, useEffect } from 'react';
+import axios from 'axios';
+import MessagesView from './MessagesView';
+import InboxListItem from './InboxListItem';
+
+// maybe I am a popup, maybe a dropdown menu thing, maybe a separate page
+interface IProps {
+  user:{
+    id: number,
+    nameFirst: string,
+    nameLast: string,
+    username: string,
+    email: string,
+    avatar: string,
+    googleId: string,
+  },
+}
+
+const Messages: FC<IProps> = ({ user }) => {
+  // get rid of hard coded clickedUser
+  const [clickedUser, setClickedUser]: any = useState({
+    id: 2,
+    nameFirst: 'Travis',
+    nameLast: 'Scott',
+    username: 'AstroJack',
+    email: 'JackBoys@gmail.com',
+    avatar: 'https://i1.sndcdn.com/avatars-000701366305-hu9f0i-t500x500.jpg',
+    googleId: 'google ID here',
+  });
+  const [threads, setThreads]: any = useState([]);
+
+  useEffect(() => {
+    // axios.......
+    setThreads([]);
+  });
+
+  return (
+    <div>
+      {threads.map((thread: any) => {
+        return <InboxListItem thread={thread} />;
+      })}
+      <MessagesView clickedUser={clickedUser} user={user} />
+    </div>
+  );
+};
+
+export default Messages;
