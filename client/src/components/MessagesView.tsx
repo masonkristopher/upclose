809a32dad284ac92ed297f9257581d8863d32bb6
NaseerHines <Hinesnaseer@gmail.com>
2020-07-14 11:40:56 -0500

    Hard coded messages are working routes need to be setup

--- client/src/components/MessagesView.tsx
+++ client/src/components/MessagesView.tsx
@@ -1,4 +1,5 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useState, useEffect } from 'react';
+import axios from 'axios';
 // maybe I am a popup, maybe a dropdown menu thing, maybe a separate page
 interface IProps {
   user: {
@@ -36,27 +37,43 @@ const fakeMessages = [
   },
   {
     id: 3,
-    message: "What's up gamer",
+    message: 'We gaming today?',
     sender_id: 3,
     recipient_id: 2,
   },
   {
     id: 4,
-    message: "What's up gamer",
+    message: 'Fortnite?',
     sender_id: 3,
     recipient_id: 2,
   },
   {
     id: 4,
-    message: 'EZ Dubs',
+    message: 'lets get these EZ Dubs',
     sender_id: 2,
     recipient_id: 3,
   },
 ];
 
 const MessagesView: FC<IProps> = ({ user, clickedUser }) => {
-  const [message, setMessage]: any = useState('');
+  const [typedMessage, setTypedMessage]: any = useState('');
   const [allMessages, setAllMessages]: any = useState(fakeMessages);
+
+  useEffect(() => {
+    axios
+      .get(`/messages/all/${user.id}/${clickedUser.id}`)
+      .then((response) => {
+        setAllMessages(response.data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+  const sendMessage = () => {
+    axios
+      .post(`/message/sendto/${clickedUser.id}/from/${user.id}`)
+      .then(() => console.log('Message was sent'))
+      .catch(error => console.log(error));
+  };
 
   return (
     <div>
@@ -64,41 +81,50 @@ const MessagesView: FC<IProps> = ({ user, clickedUser }) => {
         I am the child of beautiful Messages
       </div>
       <div>
-        {/* make this not equal to fake messages once backend is set up. */}
-        {allMessages === fakeMessages
-          ? allMessages.map((messageObj: any) => (
-            messageObj.sender_id === user.id
-              ? (
-                <div key={messageObj.id}>
-                  <img
-                    className="rounded-full mx-auto h-6 w-6"
-                    src={user.avatar}
-                    alt="avatar"
-                  />
-                  <li>{{ message }}</li>
-                </div>
-              )
-              : (
-                <div key={messageObj.id}>
-                  <img
-                    className="rounded-full mx-auto h-6 w-6"
-                    src={clickedUser.avatar}
-                    alt="avatar"
-                  />
-                  <li>{{ message }}</li>
-                </div>
-              )
+        {fakeMessages.length >= 1
+          ? allMessages.map((messageObj: {id: number, message: string, sender_id: number, recipient_id: number}) => (
+            <div key={messageObj.id}>
+              {messageObj.sender_id === user.id
+                ? (
+                  <div className="">
+                    <img
+                      className="rounded-full mx-auto h-6 w-6"
+                      src={user.avatar}
+                      alt="avatar"
+                    />
+                    <p className="bg-gray-200">{messageObj.message}</p>
+                  </div>
+                )
+                : (
+                  <div className="">
+                    <img
+                      className="rounded-full mx-auto h-6 w-6"
+                      src={clickedUser.avatar}
+                      alt="avatar"
+                    />
+                    <p className="bg-blue-200">{messageObj.message}</p>
+                  </div>
+                )}
+            </div>
           ))
           : (
             <div>Say something!</div>
           )}
       </div>
-      <div>
-        <form>
-          <input type="text" name="name" placeholder="Enter a message" />
-          <input type="submit" value="Submit" />
-          <button type="button">Send</button>
-        </form>
+      <div className="">
+        <input
+          onChange={(e) => setTypedMessage(e.target.value)}
+          className="appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+          type="text"
+          value={typedMessage}
+        />
+        <button
+          onClick={() => sendMessage()}
+          className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-blue-400 rounded shadow m-4"
+          type="button"
+        >
+          Send
+        </button>
       </div>
     </div>
   );
