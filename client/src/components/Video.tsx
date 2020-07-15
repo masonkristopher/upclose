dd348aa4064b43391589e429ee221cdca89e7646
Clayton Christenson <cchristenson.opspark@gmail.com>
2020-07-15 17:49:08 -0500

    first attempt at audio processing in video.tsx

--- client/src/components/Video.tsx
+++ client/src/components/Video.tsx
@@ -7,31 +7,43 @@ interface VideoProps {
 }
 
 const Video: FC<VideoProps> = ({ peer }) => {
-  const ref = useRef<HTMLVideoElement>(null);
+  // const ref = useRef<HTMLVideoElement>(null);
+  const [gain, setGain] = useState(0);
+  const ref: any = useRef();
   const audioCtx = new AudioContext();
   const gainNode = audioCtx.createGain();
-  const [gain, setGain]: any = useState(1);
 
   useEffect(() => {
     peer.on('stream', (stream: any) => {
       if (ref.current !== null) {
         ref.current.srcObject = stream;
+        const track = ref.current.srcObject.getAudioTracks()[0];
+        // track.enabled = false;
+        console.log(track);
         // new WebAudio context
         const source = audioCtx.createMediaStreamSource(stream);
-        source.connect(gainNode);
         gainNode.connect(audioCtx.destination);
-        gainNode.gain.setValueAtTime(0, audioCtx.currentTime);
+        source.connect(gainNode);
+        // gainNode.gain.linearRampToValueAtTime(0, 0);
+        gainNode.gain.value = gain;
+        console.log(gainNode.gain.value);
       }
     });
-  }, []);
+  }, [gain]);
+
+  // const setGain = (num: number) => {
+  //   gainNode.gain.value = ;
+  //   // gainNode.gain.linearRampToValueAtTime(num, 0);
+  //   console.log('set gain clicked!', gainNode.gain.value);
+  // };
 
   return (
     <div className="text-blue mt-4">
       <video ref={ref} playsInline autoPlay>
         <track />
       </video>
-      <button type="button" className="bg-white hover:bg-pink-100 text-gray-800 font-semibold p-1 border border-gray-400 rounded shadow float-right" onClick={() => { gainNode.gain.setValueAtTime(0, audioCtx.currentTime); }}>WEBAUDIOMUTE</button>
-      <button type="button" className="bg-white hover:bg-pink-100 text-gray-800 font-semibold p-1 border border-gray-400 rounded shadow float-right" onClick={() => { gainNode.gain.setValueAtTime(1, audioCtx.currentTime); }}>webaudioUNmute</button>
+      <button type="button" className="bg-white hover:bg-pink-100 text-gray-800 font-semibold p-1 border border-gray-400 rounded shadow float-right" onClick={() => { setGain(0); }}>WEBAUDIOMUTE</button>
+      <button type="button" className="bg-white hover:bg-pink-100 text-gray-800 font-semibold p-1 border border-gray-400 rounded shadow float-right" onClick={() => { setGain(1); }}>webaudioUNmute</button>
     </div>
   );
 };
