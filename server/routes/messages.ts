e71f003ceeb915ca85f0c0a766b53c201b7d82ea
NaseerHines <Hinesnaseer@gmail.com>
2020-07-15 16:31:40 -0500

    Routes 4 send and get messages between 2 users

--- server/routes/messages.ts
+++ server/routes/messages.ts
@@ -1,13 +1,30 @@
 import express from 'express';
-import { getAllUsersThreads } from '../db/methods';
+import { getMessagesWithOneUser, getAllUsersThreads, sendUserMessage } from '../db/methods';
 
 const messagesRouter = express.Router();
+
+messagesRouter.get('/all/:idSender/:idRecipient', (req, res) => {
+  const { idSender, idRecipient } = req.params;
+  getMessagesWithOneUser(idSender, idRecipient)
+    .then(messages => {
+      res.send(messages);
+    })
+    .catch(err => console.error(err));
+});
 
 messagesRouter.get('/threads/:id', (req, res) => {
   const { id } = req.params;
   getAllUsersThreads(id)
     .then(idArr => {
       res.send(idArr);
+    })
+    .catch(err => console.error(err));
+});
+
+messagesRouter.post('/send', (req, res) => {
+  sendUserMessage(req.body)
+    .then(() => {
+      res.send('message sent');
     })
     .catch(err => console.error(err));
 });
