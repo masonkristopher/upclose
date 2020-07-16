e41d548e1c7ca9fcb3764dd6fdda8577cc314b0a
Will Preble <wpreble1@gmail.com>
2020-07-15 20:01:55 -0500

    Refactor for positions, extract constants and services

--- client/src/components/House.tsx
+++ client/src/components/House.tsx
@@ -1,36 +1,12 @@
 /* eslint-disable max-len */
-import React, { FC, ReactElement, useState, useEffect, useRef } from 'react';
-import PartyGoer from './PartyGoer';
-import Player from './Player';
+import React, { FC, ReactElement, useState } from 'react';
+
 import Room from './Room';
 
-// type layout = 'red' | 'green' | 'blue' | 'yellow';
-type dir = 'LEFT' | 'UP' | 'RIGHT' | 'DOWN';
-type layout = 'red' | 'green' | 'blue' | 'yellow';
-
-interface iRoom {
-  name: layout,
-  xChange: layout,
-  yChange: layout,
-}
-
-interface iHouse {
-  red: iRoom,
-  blue: iRoom,
-  green: iRoom,
-  yellow: iRoom,
-}
+import { User, house } from '../services/constants';
 
 interface HouseProps {
-  user: {
-    id: number,
-    nameFirst: string,
-    nameLast: string,
-    username: string,
-    email: string,
-    avatar: string,
-    googleId: string,
-  };
+  user: User;
   setRoomID: any;
   positions: any;
   setPositions: any;
@@ -44,38 +20,15 @@ const House: FC<HouseProps> = ({
   setPositions,
   socket,
 }): ReactElement => {
-  const [rooms, setRooms] = useState<iHouse>({
-    red: {
-      name: 'red',
-      xChange: 'blue',
-      yChange: 'green',
-    },
-    blue: {
-      name: 'blue',
-      xChange: 'red',
-      yChange: 'yellow',
-    },
-    green: {
-      name: 'green',
-      xChange: 'yellow',
-      yChange: 'red',
-    },
-    yellow: {
-      name: 'yellow',
-      xChange: 'green',
-      yChange: 'blue',
-    },
-  });
+  const [currRoom, setCurrRoom] = useState(house.red);
 
-  const [currRoom, setCurrRoom] = useState(rooms.red);
-
-  const changeRoom = (dir: dir) => {
+  const changeRoom = (dir: 'LEFT' | 'UP' | 'RIGHT' | 'DOWN') => {
     const { name, xChange, yChange } = currRoom;
     if (dir === 'UP' || dir === 'DOWN') {
-      setCurrRoom(rooms[yChange]);
+      setCurrRoom(house[yChange]);
       setRoomID({ oldRoom: name, newRoom: yChange });
     } else if (dir === 'LEFT' || dir === 'RIGHT') {
-      setCurrRoom(rooms[xChange]);
+      setCurrRoom(house[xChange]);
       setRoomID({ oldRoom: name, newRoom: xChange });
     }
   };
@@ -86,7 +39,7 @@ const House: FC<HouseProps> = ({
         {`${user.username} is in Room: ${currRoom.name}`}
       </div>
       <div className="mx-auto bg-gray-200 h-500 w-500 border-solid border-black" id="house-container">
-        <Room name="Room 1" layout={currRoom.name} user={user} changeRoom={changeRoom} positions={positions} setPositions={setPositions} socket={socket} />
+        <Room name={currRoom.name} user={user} changeRoom={changeRoom} positions={positions} setPositions={setPositions} socket={socket} />
       </div>
     </div>
   );
