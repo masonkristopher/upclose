7d4863bc76a612207306426da5247991c9e2238e
masonkristopher <masonk000@gmail.com>
2020-07-16 11:38:31 -0500

    add style to chat component

--- client/src/components/ChatSend.tsx
+++ client/src/components/ChatSend.tsx
@@ -45,17 +45,16 @@ const ChatSend: FC<ChatSendProps> = ({ user, socket }) => {
   }, []);
 
   return (
-    <div className="relative text-blue border rounded p-4 bg-gray-200 h-chat w-64">
+    <div className="relative p-6 border rounded bg-gray-200 h-chat w-500">
       <div className="overflow-y-auto h-40">
         {messages.reverse().map((msg: any) => {
           const key = `${msg.messageCount}-${msg.socketId}`;
           return (
             <div key={key} className="relative">
-              <img src={msg.user.avatar} alt={`Avatar for ${msg.user.username}`} className="float-left rounded-full h-5 w-5 z-10 mr-2 mt-1" />
+              {/* <img src={msg.user.avatar} alt={`Avatar for ${msg.user.username}`} className="float-left rounded-full h-5 w-5 z-10 mr-2 mt-1" /> */}
               <span className="align-middle text-xs font-bold">
                 {msg.user.username.toUpperCase()}
-                :
-                <br />
+                :&nbsp;
               </span>
               <span className="align-middle text-sm">
                 {msg.message}
@@ -69,10 +68,13 @@ const ChatSend: FC<ChatSendProps> = ({ user, socket }) => {
         })}
       </div>
       <br />
-      <div className="relative bottom-0">
-        <input id="chat-input" value={message} onKeyPress={onKeyPress} onChange={(e) => setMessage(e.target.value)} type="text" className="appearance-none shadow block w-full bg-gray-100 text-gray-700 border border-gray-400 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" />
-        <br />
-        <button onClick={(e) => sendChat(e)} type="button" className="bg-white hover:bg-pink-100 text-gray-800 font-semibold p-1 border border-gray-400 rounded shadow float-right">Send</button>
+      <div className="absolute bottom-0 py-2 w-full">
+        <input id="chat-input" value={message} onKeyPress={onKeyPress} onChange={(e) => setMessage(e.target.value)} type="text" className="appearance-none shadow float-left w-chat-input bg-gray-100 text-gray-700 border border-gray-400 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" />
+        <button onClick={(e) => sendChat(e)} type="button" className="float-left bg-white hover:bg-pink-100 text-gray-800 font-semibold p-2 mx-2 border border-gray-400 rounded-full shadow">
+          <svg className="h-6 w-6 fill-current text-gray-500" xmlns="http://www.w3.org/2000/svg">
+            <path xmlns="http://www.w3.org/2000/svg" className="heroicon-ui" d="M13 5.41V21a1 1 0 0 1-2 0V5.41l-5.3 5.3a1 1 0 1 1-1.4-1.42l7-7a1 1 0 0 1 1.4 0l7 7a1 1 0 1 1-1.4 1.42L13 5.4z" />
+          </svg>
+        </button>
       </div>
     </div>
   );
