b2c8deccd2b2187ef26dc3500e0e4b3466767814
Will Preble <wpreble1@gmail.com>
2020-07-15 20:02:55 -0500

    Refactor for positions, add calibration

--- client/src/components/PartyGoer.tsx
+++ client/src/components/PartyGoer.tsx
@@ -1,41 +1,36 @@
 /* eslint-disable max-len */
 import React, { FC, ReactElement } from 'react';
 
+import { User } from '../services/constants';
+
 interface position {
+  avatar: string,
   top: number,
   left: number,
 }
-
 interface PartyGoerProps {
   position: position;
-  user: {
-    id: number,
-    nameFirst: string,
-    nameLast: string,
-    username: string,
-    email: string,
-    avatar: string,
-    googleId: string,
-  };
+  calibration?: number;
+  // user: User;
 }
 
 const PartyGoer: FC<PartyGoerProps> = ({
-  user,
+  // user,
   position,
+  calibration = 0,
 }): ReactElement => {
-  const assignPosition = (position: position) => {
+  const assignPosition = (pos: position) => {
     return {
-      top: `${position.top}px`,
-      left: `${position.left}px`,
+      top: `${pos.top - calibration}px`,
+      left: `${pos.left}px`,
     };
   };
-
   return (
     <div className="">
       <img
         className="relative rounded-full h-10 w-10 z-10"
         style={assignPosition(position)}
-        src={user.avatar}
+        src={position.avatar}
         alt="party goer"
       />
     </div>
