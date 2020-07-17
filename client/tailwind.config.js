abcc9a980f817c7d1e38f47d70ae7e892c46be18
Will Preble <wpreble1@gmail.com>
2020-07-17 14:41:13 -0500

    Add theme colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        caviar: '#F77604',
+        avocado: '#B8D20B',
+        salmon: '#F56C57',
+        seaweed: '#231B12',
+      }
+    },
   },
   variants: {},
   plugins: [],
