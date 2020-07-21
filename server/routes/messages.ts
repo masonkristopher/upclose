81a25034fc6c5c2b2a4898b1665a93f1c5b40298
NaseerHines <Hinesnaseer@gmail.com>
2020-07-20 21:28:56 -0500

    added route to get the last message of 2 users

--- server/routes/messages.ts
+++ server/routes/messages.ts
@@ -3,14 +3,21 @@ import { getAllUserMessages, getAllUsersThreads, sendUserMessage } from '../db/m
 
 const messagesRouter = express.Router();
 
-// messagesRouter.get('/all/:idSender/:idRecipient', (req, res) => {
-//   const { idSender, idRecipient } = req.params;
-//   getMessagesWithOneUser(idSender, idRecipient)
-//     .then(messages => {
-//       res.send(messages);
-//     })
-//     .catch(err => console.error(err));
-// });
+messagesRouter.get('/:idSender/:idRecipient/last', (req, res) => {
+  const { idSender, idRecipient } = req.params;
+  getAllUserMessages(idSender, idRecipient)
+    .then((currentUserMessages) => {
+      getAllUserMessages(idRecipient, idSender)
+        .then((clickedUserMessages) => {
+          const allMessages = currentUserMessages
+            .concat(clickedUserMessages)
+            .sort((a: any, b: any) => a.id - b.id);
+          res.send(allMessages[allMessages.length - 1]);
+        })
+        .catch((error) => console.error(error));
+    })
+    .catch((err) => console.error(err));
+});
 
 messagesRouter.get('/all/:idSender/:idRecipient', (req, res) => {
   const { idSender, idRecipient } = req.params;
